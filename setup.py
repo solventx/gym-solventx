7ce6751ade93926eef9a5b31a692df7fcd68c4c9
Plathottam <splathottam@anl.gov>
2019-06-04 11:01:36 -0500

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_simulator',
+      version='0.0.1',
+      install_requires=['gym','scipy','numpy','pandas','cantera']#And any other dependencies required 'matplotlib'
+)

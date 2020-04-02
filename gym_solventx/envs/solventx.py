7b54c3a246bb28ad0fb4e5c0405547c85a1e4757
Plathottam <sibyjackgrove@gmail.com>
2020-04-02 16:08:47 -0500

    Refactor reward calculation

--- gym_solventx/envs/solventx.py
+++ gym_solventx/envs/solventx.py
@@ -47,7 +47,7 @@ class SolventXEnv(gym.Env):
        
         
         self.goals_list        = self.environment_config['goals_list']
-        self.DISCRETE_REWARD   = self.reward_config['discrete_reward']
+        self.discrete_reward   = self.reward_config['discrete_reward']
         
         self.action_dict = utilities.create_action_dict(self.variable_config,self.environment_config)
         self.observation_variables = utilities.create_variables_list(self.variable_config,self.environment_config)       
@@ -145,7 +145,7 @@ class SolventXEnv(gym.Env):
         self.max_episode_steps = min(self.environment_config['max_episode_steps'],self.spec.max_episode_steps)
         self.action_stats = {action: 0 for action in range(self.action_space.n)}         #reset action stats
         self.sx_design = solventx.solventx(config_file=self.process_config['design_config']) # instantiate object
-        x0,n_components= self.get_process() #Get initial values of variables and inputs
+        self.modules,x0,n_components= self.get_process() #Get initial values of variables and inputs
         self.sx_design.create_var_space(x0,n_components) #define variable space
         print(f'Var space:{self.sx_design.combined_var_space},Number of inputs:{n_components}')
         self.initialize_design_variables()
@@ -177,7 +177,7 @@ class SolventXEnv(gym.Env):
                     config_key = key
         
         if config_key:
-            print(f'Found the following process config:{key}')            
+            print(f'Found the following process config:{config_key}')            
         else:
             raise ValueError(f'No configuration found for input:{input_components},strip:{strip_components}!')            
        
@@ -191,7 +191,7 @@ class SolventXEnv(gym.Env):
             print(f'Module {key}:{module["strip_group"]}')
         print(f'x0:{x}')
         
-        return x,n_components
+        return modules,x,n_components
     
     def initialize_design_variables(self):
         """Initialize design variables."""
@@ -218,6 +218,52 @@ class SolventXEnv(gym.Env):
         #except:
             #print('Solvent extraction design evaluation failed -  initialization failed!')
         
+    
+    def get_metrics(self):
+        """Extract and return metrics for each element."""
+        
+        metrics = {}
+        for metric in self.reward_config['metrics']: #Extract value for each metric
+            metrics.update({metric:{}})
+            for element in self.sx_design.confDict['modules']['output']['strip']: #Extract value for each element
+                if metric == 'recovery':
+                   metric_value = self.obj.recovery['Strip-0'][self.obj.ree.index(element)] #Recovery 
+                elif metric == 'purity':
+                   metric_value = self.obj.purity['Strip-0'][self.obj.ree.index(element)] #Purity
+                else:
+                    raise ValueError(f'{metric} is an invalid metric!')
+               
+                metrics[metric].update({element:metric_value}) #{'recovery':{'Nd':0.1,'Pr':0.1}}
+    
+    def get_reward(self):
+        """Calculate and return reward."""
+        
+        rewards = []
+        metric_sum = 0.0
+        metric_dict = self.get_metrics()
+        
+        for goal in self.environment_config['goals']:
+            if goal not in self.reward_config['metrics']:
+                raise ValueError(f'{goal} is not found in reward config!')
+            
+            metric_type= metric_dict[goal]
+                
+            for element,metric in metric_type.items():
+                for _,metric_config in self.reward_config['metrics'][goal].item():
+                    if metric < metric_config['threshold']:
+                        if isinstance(metric_config['threshold']['reward'],(int,float)):
+                            metric = metric_config['threshold']['reward']
+                        elif isinstance(metric_config['threshold']['reward'],str):
+                            metric = eval(metric_config['threshold']['reward'])
+                        else:
+                            raise(f'{metric_config["threshold"]["reward"]} is an invalid reward!')
+                metric_sum = metric_sum +  metric
+            
+            rewards.append(metric_sum) #Append reward for each goal
+            
+        return sum(rewards) #Sum rewards for all goals
+    
+    
     def render(self, mode='human', create_graph_every=False):
         '''
         create_graph_every: steps per graph generation
@@ -236,10 +282,10 @@ class SolventXEnv(gym.Env):
         
         return self.sx_design.x
 
-    def show_action_stats(self,SHOW_PLOT=False):
+    def show_action_stats(self,show_plot=False):
         """Show actions count for episode."""
     
-        if SHOW_PLOT:
+        if show_plot:
             action_list = list(self.action_stats.keys()) 
             action_count = list(self.action_stats.values())
 
@@ -254,203 +300,7 @@ class SolventXEnv(gym.Env):
             
         return self.action_stats
 
-    def get_reward(self):
-        reward = []
-        if not self.goals_list:
-          obj = self.obj
-          reward = [(obj.strip_pur[obj.ree.index('Nd')] * obj.strip_recov[obj.ree.index('Nd')]) -\
-            (obj.strip_pur[obj.ree.index('Pr')] * obj.strip_recov[obj.ree.index('Pr')]) +\
-            (obj.scrub_pur[obj.ree.index('Pr')] * obj.scrub_recov[obj.ree.index('Pr')]) -\
-            (obj.scrub_pur[obj.ree.index('Nd')] * obj.scrub_recov[obj.ree.index('Nd')]) +\
-            (obj.ext_pur  [obj.ree.index('Pr')] * obj.ext_recov  [obj.ree.index('Pr')]) -\
-            (obj.ext_pur  [obj.ree.index('Nd')] * obj.ext_recov  [obj.ree.index('Nd')])]
-        else:
-          if 'All' in self.goals_list:
-            self.goals_list = ['Purity', 'Recovery', 'Stages', 'OA Extraction', 'OA Scrub', \
-              'OA Strip', 'Recycle', 'Profit']
-
-          if False in self.obj.stage_status: #invalid state
-            reward.append(-25)
-
-          else:
-            # Nd recovery
-            recovery = self.obj.recovery['Strip-0'][self.obj.ree.index('Nd')]
-            # Nd purity
-            purity = self.obj.purity['Strip-0'][self.obj.ree.index('Nd')]
-            #print(recovery)
-            #print(purity)
-            for goal in self.goals_list:
-              if goal == 'Purity':
-                  if purity < self.purity_threshold:
-                    if self.DISCRETE_REWARD:
-                        reward.append(-1)
-                    else:
-                        reward.append(-abs(1-purity))
-                  else:
-                    if self.DISCRETE_REWARD:
-                        reward.append(1)
-                    else:
-                        if recovery < self.recovery_threshold:
-                            reward.append(0)
-                        else:
-                            reward.append(min(1, purity))
-
-              if goal == 'Recovery':
-                
-                if self.DISCRETE_REWARD:
-                  threshold     = [   0.5, 0.7, 0.8, 0.9, 0.99]
-                  rec_rewards   = [0,  .5,  .7,  .8,  .9,    1] #0 if no threshold met
-                  i = -1 #reward index -1
-                  for index, val in enumerate(threshold):
-                    if recovery > val:
-                      i = index
-                  reward.append(rec_rewards[1+i])
-                else:
-                  if recovery < self.recovery_threshold:
-                    reward.append(-abs(1-recovery))
-                    
-                  else:
-                    if purity < self.purity_threshold:
-                        reward.append(0)
-                    else:
-                        reward.append(min(1, recovery))
-
-              if goal == 'Stages': #lower stages = better
-                col_rewards = []
-
-                cols       = self.obj.variables.tolist()[-3:] #extraction, scrub, strip
-                for i, stages in enumerate(cols):
-                  if stages == 0:
-                    raise ValueError('Invalid stage structure for', 
-                      ['Extraction', 'Scrub', 'Strip'][i], 'column')
-                  else:
-                    if self.DISCRETE_REWARD:
-                      bounds = self.bounds[['Extraction', 'Scrub', 'Strip'][i]]
-                      lower  = bounds['lower']
-                      upper  = bounds['upper']
-                      norm_stages = normalize([lower, stages, upper], 0, 1)[1]
-
-                      if norm_stages < 0.10:
-                        col_rewards.append(1)
-                      elif 0.10 <= norm_stages < 0.25:
-                        col_rewards.append(.75)
-                      elif 0.25 <= norm_stages < 0.50:
-                        col_rewards.append(0.5)
-                      elif 0.50 <= norm_stages < 0.75:
-                        col_rewards.append(0.25)
-                      else:
-                        col_rewards.append(0)
-                    else:
-                      col_rewards.append(abs(1/stages))
-
-                reward.append(sum(col_rewards)/3)
-
-              if goal == 'OA Extraction': #lower = better
-                bounds = self.bounds['OA Extraction']
-                lower  = bounds['lower']
-                upper  = bounds['upper']
-                current = self.envstate['OA Extraction']
-
-                norm_current = normalize([lower, current, upper], 0, 1)[1]
-                if self.DISCRETE_REWARD:
-                  if norm_current < 0.10:
-                    reward.append(1)
-                  elif 0.10 <= norm_current < 0.25:
-                    reward.append(.75)
-                  elif 0.25 <= norm_current < 0.50:
-                    reward.append(0.5)
-                  elif 0.50 <= norm_current < 0.75:
-                    reward.append(0.25)
-                  else:
-                    reward.append(0)
-                else:
-                  reward.append(abs(1-norm_current))
-
-              if goal == 'OA Scrub': #higher = better
-                bounds = self.bounds['OA Scrub']
-                lower  = bounds['lower']
-                upper  = bounds['upper']
-                current = self.envstate['OA Scrub']
-
-                norm_current = normalize([lower, current, upper], 0, 1)[1]
-                if self.DISCRETE_REWARD:
-                  threshold       = [   0.1, 0.25, 0.5, 0.75, 0.9, .95]
-                  scrub_rewards   = [0,  .1,  .25, 0.5, 0.75,  .9,   1] #0 if no threshold met
-                  i = -1 #reward index -1
-                  for index, val in enumerate(threshold):
-                    if norm_current > val:
-                      i = index
-                  reward.append(scrub_rewards[1+i])
-                else:
-                  reward.append(norm_current)
-
-              if goal == 'OA Strip': #higher = better
-                bounds = self.bounds['OA Strip']
-                lower  = bounds['lower']
-                upper  = bounds['upper']
-                current = self.envstate['OA Strip']
-
-                norm_current = normalize([lower, current, upper], 0, 1)[1]
-                if self.DISCRETE_REWARD:
-                  threshold       = [   0.1, 0.25, 0.5, 0.75, 0.9, .95]
-                  strip_rewards   = [0,  .1,  .25, 0.5, 0.75,  .9,   1] #0 if no threshold met
-                  i = -1 #reward index -1
-                  for index, val in enumerate(threshold):
-                    if norm_current > val:
-                      i = index
-                  reward.append(strip_rewards[1+i])
-                else:
-                  reward.append(norm_current)
-
-              if goal == 'Recycle': #lower = better
-                recycle = self.envstate['Recycle']
-
-                if self.DISCRETE_REWARD:
-                  if recycle < 0.10:
-                    reward.append(1)
-                  elif 0.10 <= recycle < 0.25:
-                    reward.append(.75)
-                  elif 0.25 <= recycle < 0.50:
-                    reward.append(0.5)
-                  elif 0.50 <= recycle < 0.75:
-                    reward.append(0.25)
-                  else:
-                    reward.append(0)
-                else:
-                  reward.append(abs(1-recycle))
-
-              if goal == 'Profit':
-                if hasattr(self.obj, 'psuedo_profit'):
-                  profit = self.obj.psuedo_profit
-                  max_profit = self.obj.max_profit
-                  min_profit = self.obj.min_profit
-
-                  #normalize values between 0-2, making profit second most important goal
-                  norm_profit = normalize([min_profit, profit, max_profit], 0, 2)[1]
-
-                  if self.DISCRETE_REWARD:
-                    if norm_profit <= .25:
-                      reward.append(-1)
-                    elif .25 < norm_profit <= .5:
-                      reward.append(0)
-                    elif .5 < norm_profit <= .75:
-                      reward.append(.25)
-                    elif .75 < norm_profit <= 1:
-                      reward.append(.5)
-                    elif 1 < norm_profit <= 1.25:
-                      reward.append(.75)
-                    elif 1.25 < norm_profit <= 1.50:
-                      reward.append(.85)
-                    elif 1.50 < norm_profit <= 1.75:
-                      reward.append(.95)
-                    elif 1.75 < norm_profit:
-                      reward.append(1)
-                  else:
-                    reward.append(norm_profit)
-
-
-        #print(reward, truncate_number(sum(reward))) #logging
-        return truncate_number(sum(reward)) #remove imprecision
+    
     
     def show_design_performance(self):
         """Show the purity and recovery of current recovery."""
